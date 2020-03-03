f429de86ccc6633a9fa183321a0725c8d2b7ed4f
Anahita Esfandiaryfard <anita.kntu@gmail.com>
2020-03-03 21:25:04 +0330

    + Unit test for set user data added

---
+++ apps/user_data/tests/set_user_data_tests.py
@@ -0,0 +1,29 @@
+import unittest
+
+from webtest import TestApp
+
+from app import app
+
+
+class UserDataUnitTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.app = TestApp(app)
+
+    def test_create_user_json_file(self):
+        response = self.app.post(
+            '/api/setUserData?username=testuser',
+            {'name': '1', 'value': '2'}
+        )
+        assert response.status_int == 200
+
+    def test_update_json_with_same_name(self):
+        response = self.app.post(
+            '/api/setUserData?username=testuser',
+            {'name': '1', 'value': '3'}
+        )
+        assert response.status_int == 200
+
+
+if __name__ == '__main__':
+    unittest.main( )
